0bbf397e73d572f3d281e39b6726c83dcd3a0782
iamr2003 <roughtar@gmail.com>
2023-06-09 21:35:23 -0400

    made a thing, but it works without guards which is annoying

--- main.cpp
+++ main.cpp
@@ -1,34 +1,46 @@
+#include <chrono>
+#include <mutex>
 #include <thread>
 #include <vector>
-
+#include <iostream>
 // have a lock then allow things to access it
 //
 struct Bank {
   int val;
-  bool lock;
   Bank() : val{0} {}
 };
 
-void withdraw() {}
+Bank b;
+std::mutex b_mutex;
 
-void deposit() {}
+void withdraw(int val) {
+  // const std::lock_guard<std::mutex> lock(b_mutex);
+  b.val -= val;
+}
+
+void deposit(int val) {
+  // const std::lock_guard<std::mutex> lock(b_mutex);
+  b.val += val;
+}
 // Start with one account and value, and multiple accessors
 int main() {
-  int num_transactions = 10;
+  int num_transactions = 1000;
   std::vector<std::thread> threads;
   // use constant transaction
+  // I don't think this is a good test, needs to mix them more
 
-  for(int i=0;i<num_transactions;++i){
-    std::thread t(withdraw);
+  for (int i = 0; i < num_transactions; ++i) {
+    std::thread t(withdraw,10);
     threads.push_back(std::move(t));
   }
 
-  for(int i=0;i<num_transactions;++i){
-    std::thread t(deposit);
+  for (int i = 0; i < num_transactions; ++i) {
+    std::thread t(deposit,11);
     threads.push_back(std::move(t));
   }
-  
-  for(auto &t:threads){
+
+  for (auto &t : threads) {
     t.join();
   }
+  std::cout << b.val; 
 }
