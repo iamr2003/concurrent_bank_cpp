79e4c6f1f8aaa3e40e5dbdc708cf289cebced985
iamr2003 <roughtar@gmail.com>
2023-07-02 23:07:35 -0400

    wrote out a simple unique ptr demo

--- smart_ptr_ex.cpp
+++ smart_ptr_ex.cpp
@@ -1,23 +1,112 @@
+#include <iostream>
+#include <iterator>
 #include <memory>
+#include <ostream>
+#include <vector>
 
-struct DummyType{
+struct DummyType {
   int a;
   double b;
   double c;
 };
 
-std::unique_ptr<DummyType> mover(std::unique_ptr<DummyType> ptr_in){ 
-  return std::make_unique<DummyType>(*ptr_in);
+// let's remake a data structure that would be heap allocated using unique
+// smart_ptrs Linked List
+template <class T> class linked_list {
+  // singly linked for now
+  struct Node {
+    T val;
+    std::unique_ptr<Node> next;
+    Node(T _val) : val(_val), next(nullptr){};
+  };
+
+private:
+  // No explicit destructor needed!
+  std::unique_ptr<Node> head;
+
+public:
+  linked_list() : head(nullptr){};
+
+  // TODO copy constructor
+  //  linked_list(linked_list<T> & other){
+  //
+  //  }
+
+  void push_back(T val) {
+    // bc we will be breaking the managed ptr behavior, have to grab the regular
+    // ptr for iterating
+    auto curr = head.get();
+    if (!curr) {
+      head = std::make_unique<Node>(val);
+      return;
+    }
+    while (curr->next) {
+      curr = curr->next.get();
+    }
+    curr->next = std::make_unique<Node>(val);
+  }
+
+  T pop_back() {
+    auto curr = head.get();
+    // setting a unique ptr to nullptr auto deletes content
+    if (!curr) {
+      return T();
+    }
+    if (!curr->next) {
+      T temp = head->val;
+      head = nullptr;
+      return temp;
+    }
+    while (curr->next->next) {
+      curr = curr->next.get();
+    }
+
+    T temp = curr->next->val;
+    curr->next = nullptr;
+    return temp;
+  }
+
+  void print() {
+    auto curr = head.get();
+    while (curr->next) {
+      std::cout << curr->val << " ";
+      curr = curr->next.get();
+    }
+    std::cout << curr->val << " ";
+    std::cout << std::endl;
+  }
+};
+
+std::unique_ptr<DummyType> mover(std::unique_ptr<DummyType> ptr_in) {
+  auto here = std::make_unique<DummyType>(*ptr_in);
+  return here;
 }
 
-
-//small project getting familiar with smart_ptrs
-int main (int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  // basic testing
   std::unique_ptr<DummyType> ptr = std::make_unique<DummyType>();
   ptr->a = -1;
   ptr->b = 3.14;
   ptr->c = 6.28;
 
+  std::unique_ptr<DummyType> passed_around = mover(std::move(ptr));
+
+  std::cout << passed_around->a << std::endl;
+  // invalidated after move
+  std::cout << (ptr == nullptr) << std::endl;
+
+  // linked list testing
+  std::vector<int> truth = {1, 2, 3, 4, 5, 6};
+  linked_list<int> lst;
+
+  for (auto elem : truth) {
+    lst.push_back(elem);
+  }
+  lst.print();
+
+  for (auto elem : truth) {
+    std::cout << lst.pop_back() << " ";
+  }
+  std::cout << std::endl;
   return 0;
 }
